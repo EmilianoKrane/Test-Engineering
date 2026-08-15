1adea0b5c6f31a947fdba68d15243dc779684928
Emiliano Molina <emilianomolinavaldes@gmail.com>
2026-08-14 18:09:45 -0600

    feat: firmware test pulsarc6 before initial function checking

--- TestBench UE0118 Pulsar Neopixel Shield/ue0118_pulsar_neopixel_test_probe/ue0118_pulsar_neopixel_test_probe.ino
+++ TestBench UE0118 Pulsar Neopixel Shield/ue0118_pulsar_neopixel_test_probe/ue0118_pulsar_neopixel_test_probe.ino
@@ -8,7 +8,7 @@
 
 #define BOTON_PIN 21  // Botón de Entrada
 
-#define NUMPIXELS 258 // Popular NeoPixel ring size
+#define NUMPIXELS 258  // Popular NeoPixel ring size
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel neopixels(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
@@ -16,10 +16,10 @@ Adafruit_NeoPixel neopixels(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
 #define DELAYVAL 50  // Time (in milliseconds) to pause between pixels
 
 // Dejamos una intensidad fija (puedes ajustarla de 0 a 255)
-int intensity = 3; 
+int intensity = 3;
 
 // Variable para alternar la salida (0 = Arnés, 1 = Punta de Prueba)
-int salidaActiva = 0; 
+int salidaActiva = 0;
 
 // Variables para evitar el "rebote" (debounce) del botón
 bool estadoBoton = HIGH;
@@ -28,6 +28,8 @@ unsigned long ultimoTiempoRebote = 0;
 unsigned long retardoRebote = 50;  // 50 milisegundos
 
 void setup() {
+  Serial.begin(115200);
+
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
 #endif
@@ -47,6 +49,8 @@ void setup() {
   pixels.show();
   neopixels.clear();
   neopixels.show();
+
+  Serial.println("Inicializado...");
 }
 
 // Función que revisa si el botón fue presionado
@@ -63,11 +67,11 @@ void revisarBoton() {
 
       // Si el botón está en LOW, es que fue presionado
       if (estadoBoton == LOW) {
-        
+
         // Alternamos entre la salida 0 y la salida 1 (Toggle)
         salidaActiva = !salidaActiva;
 
-        // Limpiamos ambas tiras de inmediato para que no se queden 
+        // Limpiamos ambas tiras de inmediato para que no se queden
         // LEDs encendidos de la animación anterior al hacer el cambio
         pixels.clear();
         pixels.show();
@@ -97,7 +101,7 @@ void loop() {
       neopixels.setPixelColor(i, neopixels.Color(0, intensity, 0));
       neopixels.show();
     }
-    smartDelay(DELAYVAL);  
+    smartDelay(DELAYVAL);
   }
 
   // Color Rojo
