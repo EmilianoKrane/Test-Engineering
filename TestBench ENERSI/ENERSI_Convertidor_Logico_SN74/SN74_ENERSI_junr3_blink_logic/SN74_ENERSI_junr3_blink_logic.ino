47cb829e9955c07b01e905f0e118aa2ff3938764
Emiliano Molina <emilianomolinavaldes@gmail.com>
2026-09-04 17:14:15 -0600

    feat: firmware junr3 and pulsarc6 with input and output gpios by json

--- TestBench ENERSI/ENERSI_Convertidor_Logico_SN74/SN74_ENERSI_junr3_blink_logic/SN74_ENERSI_junr3_blink_logic.ino
+++ TestBench ENERSI/ENERSI_Convertidor_Logico_SN74/SN74_ENERSI_junr3_blink_logic/SN74_ENERSI_junr3_blink_logic.ino
@@ -1,41 +1,130 @@
 /*
-Este firmware se carga en la JUNR3 para dar salidas digitales de 0-5V
+Firmware SN74 ENERSI. La JUNR3 se encarga de entregar dos salidas digitales oscilando entre 
+0 - 5V (B1 y B3). De igual forma, realiza lecturas analógicas en los pines B2 y B4. 
 */
 
 #include "Arduino.h"
+#include <ArduinoJson.h>
 #include <HardwareSerial.h>
 
-#define A1_PIN PD2
-#define A2_PIN PD3
-#define A3_PIN PD4
-#define A4_PIN PD5
+#define B1_PIN 14 // A0
+#define B2_PIN 15
+#define B3_PIN 16
+#define B4_PIN 17
 
-const int delay_ms = 2000;
+// ==== DECLARACIÓN DE OBJETO JSON ====
+StaticJsonDocument<1024> receiveJSON;
+StaticJsonDocument<1024> sendJSON;
+
+const float ADC_RESOLUTION = 1024.0;
+const float V_REF = 5.0;
+
+// ==== FUNCIONES DE UTILIDAD ====
+// Función auxiliar para generar el JSON basado en el voltaje
+void getStatusJSON(String pinName, float voltage) {
+
+  String status = (voltage >= 1.2 && voltage <= 1.7) ? "OK" : "ERROR";
+
+  StaticJsonDocument<256> doc;
+  doc.clear();
+  doc["pin"] = pinName;
+  doc["voltage"] = String(voltage, 2);
+  doc["status"] = status;
+  serializeJson(doc, Serial);
+  Serial.println();
+}
 
 void setup() {
   Serial.begin(115200);
-  delay(100);
+  delay(1000);
 
-  pinMode(A1_PIN, OUTPUT);
-  pinMode(A2_PIN, OUTPUT);
-  pinMode(A3_PIN, OUTPUT);
-  pinMode(A4_PIN, OUTPUT);
+  sendJSON.clear();
+  sendJSON["System"] = "Ready";
+  sendJSON["Module"] = "PULSAR C6 logic converter ENERSI";
+  serializeJson(sendJSON, Serial);
+  Serial.println();
 
-  Serial.println("Serial inicializado en JUNR3");
+  // ==== DECLARACIÓN DE ENTRADAS Y SALIDAS ====
+  pinMode(B1_PIN, OUTPUT);
+  pinMode(B2_PIN, INPUT);
+  pinMode(B3_PIN, OUTPUT);
+  pinMode(B4_PIN, INPUT);
 }
 
-void loop() {
-  Serial.println("Estado ALTO");
-  digitalWrite(A1_PIN, HIGH);
-  digitalWrite(A2_PIN, HIGH);
-  digitalWrite(A3_PIN, HIGH);
-  digitalWrite(A4_PIN, HIGH);
-  delay(delay_ms);
 
-  Serial.println("Estado BAJO");
-  digitalWrite(A1_PIN, LOW);
-  digitalWrite(A2_PIN, LOW);
-  digitalWrite(A3_PIN, LOW);
-  digitalWrite(A4_PIN, LOW);
-  delay(delay_ms);
+void loop() {
+
+  if (Serial.available()) {
+
+    String inputJSON = Serial.readStringUntil('\n');
+    DeserializationError error = deserializeJson(receiveJSON, inputJSON);
+
+    if (error) {
+      sendJSON.clear();
+      sendJSON["status"] = "FAIL";
+      sendJSON["error"] = String("Invalid JSON: ") + error.c_str();
+      serializeJson(sendJSON, Serial);
+      Serial.println();
+    } else {
+
+      String Function = receiveJSON["Function"];
+
+      int opc = 0;
+      if (Function == "ping") opc = 1;             // {"Function":"ping"}
+      else if (Function == "blink") opc = 2;       // {"Function":"blink"}
+      else if (Function == "analogRead") opc = 3;  // {"Function":"analogRead"}
+
+      switch (opc) {
+        case 1:
+          {
+            sendJSON.clear();
+            sendJSON["ping"] = "pong";
+            serializeJson(sendJSON, Serial);
+            Serial.println();
+            break;
+          }
+
+        case 2:
+          {
+            sendJSON.clear();
+            int delay_ms = 500;
+            for (int i = 0; i < 5; i++) {
+              digitalWrite(B1_PIN, HIGH);
+              digitalWrite(B3_PIN, HIGH);
+              delay(delay_ms);
+              digitalWrite(B1_PIN, LOW);
+              digitalWrite(B3_PIN, LOW);
+              delay(delay_ms);
+            }
+
+            sendJSON["status"] = "finished blink";
+            serializeJson(sendJSON, Serial);
+            Serial.println();
+            break;
+          }
+
+        case 3:
+          {
+            sendJSON.clear();
+            int delay_ms = 500;
+            for (int i = 0; i < 10; i++) {
+
+              int raw_b2 = analogRead(B2_PIN);
+              int raw_b4 = analogRead(B4_PIN);
+
+              float voltage_b2 = (raw_b2 / ADC_RESOLUTION) * V_REF;
+              float voltage_b4 = (raw_b4 / ADC_RESOLUTION) * V_REF;
+              getStatusJSON("B2", voltage_b2);
+              getStatusJSON("B4", voltage_b4);
+              delay(delay_ms);
+            }
+
+            sendJSON["status"] = "finished reading";
+            serializeJson(sendJSON, Serial);
+            Serial.println();
+            break;
+          }
+      }
+    }
+  }
 }
--- TestBench ENERSI/ENERSI_Convertidor_Logico_SN74/SN74_ENERSI_pulsarc6_reader_logic_states/SN74_ENERSI_pulsarc6_reader_logic_states.ino
+++ TestBench ENERSI/ENERSI_Convertidor_Logico_SN74/SN74_ENERSI_pulsarc6_reader_logic_states/SN74_ENERSI_pulsarc6_reader_logic_states.ino
@@ -1,32 +1,22 @@
 /*
-Este firmware se carga en la pulsar c6 para leer la salida del convertidor logico sn74
-por medio de un divisor de tensión, el umbral de valores validos se ajusta segun la salida 
-obtenida bajando 3.3 a la mitad como estado alto.
+Firmware SN74 ENERSI. La pulsar se encarga de entregar dos salidas digitales oscilando 
+entre 0V y 3.3V (A2 y A4), y puede realizar lecturas analogicas en A1 y A3
 */
 
+#include "Arduino.h"
 #include <ArduinoJson.h>
 #include <HardwareSerial.h>
 
-#define INPUT_A0 0
-#define INPUT_A1 1
-#define INPUT_A2 2
-#define INPUT_A3 3
+#define A1_PIN 0
+#define A2_PIN 1
+#define A3_PIN 2
+#define A4_PIN 3
 
-// ==== Declaración de variables
-String JSON_entrada;
-StaticJsonDocument<200> receiveJSON;
+// ==== DECLARACIÓN DE OBJETO JSON ====
+StaticJsonDocument<1024> receiveJSON;
+StaticJsonDocument<1024> sendJSON;
 
-String JSON_lectura;  // Variable que envía el JSON de datos
-StaticJsonDocument<200> sendJSON;
-
-// Resolución del ADC y voltaje de referencia del ESP32-C6
-const float ADC_RESOLUTION = 4095.0;
-const float V_REF = 3.3;
-
-void setup() {
-  Serial.begin(115200);
-}
-
+// ==== FUNCIONES DE UTILIDAD ====
 // Función auxiliar para generar el JSON basado en el voltaje
 void getStatusJSON(String pinName, float voltage) {
 
@@ -41,12 +31,32 @@ void getStatusJSON(String pinName, float voltage) {
   Serial.println();
 }
 
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  sendJSON.clear();
+  sendJSON["System"] = "Ready";
+  sendJSON["Module"] = "PULSAR C6 logic converter ENERSI";
+  serializeJson(sendJSON, Serial);
+  Serial.println();
+
+  // ==== DECLARACIÓN DE ENTRADAS Y SALIDAS ====
+  pinMode(A1_PIN, INPUT);
+  pinMode(A2_PIN, OUTPUT);
+  pinMode(A3_PIN, INPUT);
+  pinMode(A4_PIN, OUTPUT);
+}
+
+
 void loop() {
 
   if (Serial.available()) {
 
-    JSON_entrada = Serial.readStringUntil('\n');
-    DeserializationError error = deserializeJson(receiveJSON, JSON_entrada);
+    String inputJSON = Serial.readStringUntil('\n');
+    DeserializationError error = deserializeJson(receiveJSON, inputJSON);
 
     if (error) {
       sendJSON.clear();
@@ -57,48 +67,59 @@ void loop() {
     } else {
 
       String Function = receiveJSON["Function"];
+
       int opc = 0;
-      if (Function == "ping") opc = 1;     // {"Function":"ping"}
-      else if (Function == "tx") opc = 2;  // {"Function":"tx"}
+      if (Function == "ping") opc = 1;             // {"Function":"ping"}
+      else if (Function == "blink") opc = 2;       // {"Function":"blink"}
+      else if (Function == "analogRead") opc = 3;  // {"Function":"analogRead"}
 
       switch (opc) {
-
         case 1:
           {
             sendJSON.clear();
             sendJSON["ping"] = "pong";
-            serialiazeJson(sendJSON, Serial);
-            Serial.pritnln();
-            1 break;
+            serializeJson(sendJSON, Serial);
+            Serial.println();
+            break;
+          }
+
+        case 2:
+          {
+            sendJSON.clear();
+            int delay_ms = 500;
+            for (int i = 0; i < 5; i++) {
+              digitalWrite(A2_PIN, HIGH);
+              digitalWrite(A4_PIN, HIGH);
+              delay(delay_ms);
+              digitalWrite(A2_PIN, LOW);
+              digitalWrite(A4_PIN, LOW);
+              delay(delay_ms);
+            }
+
+            sendJSON["status"] = "finished blink";
+            serializeJson(sendJSON, Serial);
+            Serial.println();
+            break;
+          }
+
+        case 3:
+          {
+            sendJSON.clear();
+            int delay_ms = 500;
+            for (int i = 0; i < 10; i++) {
+              float voltage_a1 = analogReadMilliVolts(A1_PIN) / 1000.0;
+              float voltage_a3 = analogReadMilliVolts(A3_PIN) / 1000.0;
+              getStatusJSON("A1", voltage_a1);
+              getStatusJSON("A2", voltage_a3);
+              delay(delay_ms);
+            }
+
+            sendJSON["status"] = "finished reading";
+            serializeJson(sendJSON, Serial);
+            Serial.println();
+            break;
           }
       }
     }
   }
-
-  delay(500);
 }
-
-
-
-/*
-
-  // 1. Leer los pines analógicos
-  int raw_a0 = analogRead(INPUT_A0);
-  int raw_a1 = analogRead(INPUT_A1);
-  int raw_a2 = analogRead(INPUT_A2);
-  int raw_a3 = analogRead(INPUT_A3);
-
-  // 2. Convertir las lecturas a voltaje
-  float voltage_a0 = analogReadMilliVolts(INPUT_A0) / 1000.0;
-  float voltage_a1 = (raw_a1 / ADC_RESOLUTION) * V_REF;
-  float voltage_a2 = (raw_a2 / ADC_RESOLUTION) * V_REF;
-  float voltage_a3 = (raw_a3 / ADC_RESOLUTION) * V_REF;
-
-  // 3. Evaluar el rango e imprimir el JSON
-  getStatusJSON("A0", voltage_a0);
-  getStatusJSON("A1", voltage_a1);
-  getStatusJSON("A2", voltage_a2);
-  getStatusJSON("A3", voltage_a3);
-
-
-*/
